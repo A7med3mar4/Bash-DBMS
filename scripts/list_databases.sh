cef726a607af17a8920e626d9f5224f0a4a226fb
Ahmed Omar <ahmed6751241@gmail.com>
2025-12-26 17:10:12 +0200

    Eidt:frist line in list_databases.sh

--- scripts/list_databases.sh
+++ scripts/list_databases.sh
@@ -1,12 +1,9 @@
-
 #!/bin/bash
-
 # where databases are stored
 db_path="./Databases"
 
 # Check if Databases directory exists 
 if [[ -d "$db_path" ]]; then
-
     # Check if folder database is empty or not
     if [ -z "$(ls -A "$db_path")" ]; then
         echo "Database is empty"
@@ -14,7 +11,6 @@ if [[ -d "$db_path" ]]; then
         echo "Available Databases:"
         ls -d "$db_path"/*/
     fi
-
 else
     echo "No databases found."
 fi
